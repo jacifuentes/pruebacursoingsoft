db940829d7940a519f8861a63c0f3490629c73ee
jacifuentes <jacifuentes@icesi.edu.co>
2016-02-25 17:37:41 -0500

    Commit #close

---
+++ src/main/java/pruebaTravis/Calculadora.java
@@ -0,0 +1,8 @@
+package pruebaTravis;
+
+public class Calculadora {
+	public int multiplica(int a, int b){
+		return a*b;
+	}
+
+}
---
+++ src/test/java/pruebaTravis/TestCalculadora.java
@@ -0,0 +1,19 @@
+package pruebaTravis;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCalculadora {
+
+	@Test
+	public void testMultiplica() {
+		Calculadora calc = new Calculadora();
+		int a=2, b=3;
+		int expected = a+b;
+		int actual = calc.multiplica(a, b);
+		assertEquals(expected, actual);
+		//assertTrue(2*3 == calc.multiplica(2, 3));
+	}
+
+}

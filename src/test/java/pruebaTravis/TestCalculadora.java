5af7cfd45947a55d06ee4200e33ecf884ca79721
jacifuentes <jacifuentes@icesi.edu.co>
2016-02-26 09:28:58 -0500

    Correccion Error Test

--- src/test/java/pruebaTravis/TestCalculadora.java
+++ src/test/java/pruebaTravis/TestCalculadora.java
@@ -10,7 +10,7 @@ public class TestCalculadora {
 	public void testMultiplica() {
 		Calculadora calc = new Calculadora();
 		int a=2, b=3;
-		int expected = a+b;
+		int expected = a*b;
 		int actual = calc.multiplica(a, b);
 		assertEquals(expected, actual);
 		//assertTrue(2*3 == calc.multiplica(2, 3));
